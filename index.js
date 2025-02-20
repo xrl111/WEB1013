0a3d75275ac29e436f72cba0081ef4a3f649d429
xrl111 <buiduclan000@gmail.com>
2025-02-20 09:27:27 +0700

    de ngay 3

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const a = document.getElementById('container');
 // Tạo phần tử img
 const img = document.createElement('img');
 // Thiết lập thuộc tính src và alt cho ảnh
-img.src = 'img.jpg';
+img.src = 'dethi.jpg';
 img.alt = 'Mô tả ảnh';
 
 // Thêm các biện pháp bảo vệ ảnh
-img.style.userSelect = 'none'; // Ngăn chặn việc bôi đen
+img.style.userSelect = 'none';
+img.style.width = '100%'; // Ngăn chặn việc bôi đen
 img.draggable = false; // Vô hiệu hóa kéo thả
 img.addEventListener('contextmenu', (e) => e.preventDefault()); // Vô hiệu hóa menu chuột phải
 
